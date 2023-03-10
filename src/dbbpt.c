a6e0db23b0323f1a534812cd98f9207b3d7d271b
comolove <jungjinwoo41@gmail.com>
2023-03-10 12:29:03 +0900

    DB Project

--- src/dbbpt.c
+++ src/dbbpt.c
@@ -388,7 +388,7 @@ int db_find(int table_id,int64_t key, char* ret_val,int trx_id) {
         return -1;
     }
     buffer_close_page(table_id,0,0);
-    if(lock_acquire(table_id,key,trx_id,0) == NULL) return -1;
+   // if(lock_acquire(table_id,key,trx_id,0) == NULL) return -1;
 	hp = (HeaderPage*)buffer_read_page(table_id,0);
 	int ret = db_find_with_hp(table_id,key,ret_val,(page_t*)hp,0,NULL);
 	buffer_close_page(table_id,0,0);
